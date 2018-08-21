5dd282224501164700df8888665180d9d6a80b5a
Olivier Willems <ojwillems@gmail.com>
2018-08-21 01:12:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(names)
   end
   new_array
 end
+
+def assign_rooms(speakers)
+  room_array = []
+  speakers.each_with_index do |speaker, i|
+    room_array.push("Hello, #{speaker}! You'll be assigned to room #{i + 1}!")
+  end
+  room_array
+end
