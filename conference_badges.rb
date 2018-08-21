69a0368cb5fe9e6d90488eb8b9d977b736fc2746
Olivier Willems <ojwillems@gmail.com>
2018-08-21 01:07:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names = []
-  names.puts(badge_maker)
+  new_array = []
+  names.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  new_array
 end
